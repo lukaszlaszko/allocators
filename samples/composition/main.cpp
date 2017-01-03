72026dc3b6aeb72b2453f1075499dcd031e9c6bf
lukaszlaszko <lukaszlaszko@gmail.com>
2017-01-03 16:40:26 +0800

    Composition sample has been added.

---
+++ samples/composition/main.cpp
@@ -0,0 +1,34 @@
+#include <boost/memory/allocators.hpp>
+
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using namespace boost::memory;
+
+using my_allocator
+        = fallback_allocator<
+                free_list<
+                        stack_allocator<
+                                2048,
+                                WORD_ALIGNMENT>,
+                        4,
+                        0,
+                        32,
+                        2048 / 32>,
+                mallocator>;
+DEFINE_ALLOCATOR(my_allocator);
+
+int main(int argc, char** argv)
+{
+    INIT_ALLOCATOR();
+
+    vector<int> some_vector;
+    some_vector.push_back(1);
+    some_vector.push_back(1);
+
+    return 0;
+}
