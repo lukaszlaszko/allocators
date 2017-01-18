4fc422296862d04b064054324d376b8faaeb42b6
lukaszlaszko <lukaszlaszko@gmail.com>
2017-01-18 15:20:43 +0800

    readthedocs script adjusted.

--- docs/conf.py
+++ docs/conf.py
@@ -3,4 +3,4 @@ import subprocess, os
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    subprocess.call('cd ../docs; doxygen', shell=True)
+    subprocess.call('doxygen ../docs/doxygen.cfg', shell=True)
