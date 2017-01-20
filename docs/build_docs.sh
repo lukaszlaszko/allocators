07565ad9feeaf18b84cc6bee56bd55ae29f4de66
lukaszlaszko <lukaszlaszko@gmail.com>
2017-01-20 14:27:08 +0800

    support for version has been added.

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -50,11 +50,11 @@ repo_name=$(basename $(git remote show -n origin | grep Fetch | cut -d: -f2-))
 tag_name=$(git name-rev --name-only --tags --no-undefined HEAD 2>/dev/null | sed -n 's/^\([^^~]\{1,\}\)\(\^0\)\{0,1\}$/\1/p')
 if [[ $tag_name ]]; then
     target_dir=$target_dir/$repo_name/$tag_name
-    version=$tag_name
+    export version=$tag_name
 else
     branch_name=$(git rev-parse --abbrev-ref HEAD)
     target_dir=$target_dir/$repo_name/$branch_name
-    version=$branch_name
+    export version=$branch_name
 fi
 
 if [ ! -d "$target_dir" ]; then
