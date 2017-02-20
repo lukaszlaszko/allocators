20b596bc710367f4b10961cbe0e422cb793367bf
lukaszlaszko <lukaszlaszko@gmail.com>
2017-02-20 17:02:05 +0800

    group header added.

---
+++ src/include/boost/memory.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <boost/memory/null_allocator.hpp>
+#include <boost/memory/mallocator.hpp>
+#include <boost/memory/fallback_allocator.hpp>
+#include <boost/memory/free_list.hpp>
+#include <boost/memory/stack_allocator.hpp>
+#include <boost/memory/segregator.hpp>
+#include <boost/memory/bucketizer.hpp>
+#include <boost/memory/affix_allocator.hpp>
+#include <boost/memory/bitmapped_block.hpp>
+
